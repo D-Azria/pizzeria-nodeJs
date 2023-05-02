4365f5d5fabbef1ed84dd126c777f2deba02a96f
D-Azria <d.azria.pro@gmail.com>
2023-05-02 10:37:33 +0200

    pizz_v4_Nav.Pizz.Livreurs OK

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ import mysql from "mysql2";
 import express from "express";
 const app = express();
 
-// PARTIE CONNEXION
+//////
+//////
+//////
+//////
+//////////     PARTIE CONNECTION
+//////
+//////
+//////
+//////
 //création d'un pool de connection : gère les connection avec la BD
 const pool = mysql.createPool({
   host: "localhost",
@@ -17,6 +25,11 @@ const pool = mysql.createPool({
 // La connexion à la base de données est établie
 const promisePool = pool.promise();
 
+//
+//
+////// FONCTIONS POUR LES PIZZAS
+//
+//
 //fonction qui permet d'accéder aux données des pizzas
 async function listPizzas() {
   const [rows] = await promisePool.execute("select * from pizzas");
@@ -24,7 +37,7 @@ async function listPizzas() {
   return rows;
 }
 
-// fonction qui permet de créer une nouvelle pizza dans la BD pizzeria
+// fonction qui permet de créer une nouvelle pizza dans la BD pizzeria table pizzas
 async function newPizza(code, label, ingredients, category, price, version) {
   const [rows] = await promisePool.execute(
     "insert into pizzas(code, label, ingredients, category, price, version) values(?,?,?,?,?,?)",
@@ -42,7 +55,44 @@ async function editPizza(code, label, ingredients, category, price, version) {
   return rows;
 }
 
-// PARTIE WEB
+//
+//
+////// FONCTIONS POUR LES LIVREURS
+//
+//
+//fonction qui permet d'accéder aux données des livreurs
+async function listDM() {
+    const [rows] = await promisePool.execute("select * from delivery_men");
+    // console.log(rows);
+    return rows;
+  }
+// fonction qui permet de créer un nouveau serveur dans la BD pizzeria, table delivery_men
+async function newDM(firstname, lastname) {
+    const [rows] = await promisePool.execute(
+      "insert into delivery_men(firstname, lastname) values(?,?)",
+      [firstname, lastname]
+    );
+    return rows;
+  }
+// permet d'éditer les livreurs en fonction de l'id qui reste fixe
+async function editDM(firstname, lastname) {
+  const [rows] = await promisePool.execute(
+    "insert into delivery_men(firstname, lastname) values(?,?)",
+    [firstname, lastname]
+  );
+  return rows;
+}
+
+
+//////
+//////
+//////
+//////
+//////////     PARTIE WEB
+//////
+//////
+//////
+//////
 //démarrage du serveur
 const port = 5002;
 app.listen(port, () => {
@@ -57,11 +107,18 @@ app.use("/public", express.static("public"));
 // permet de traiter les données d'url
 app.use(express.urlencoded({ extended: true }));
 
-//Page d'accueil de l'administration de la pizzeria
+//
+////// Page d'accueil de l'administration de la pizzeria
+//
 app.get("/admin-app/", async (req, res) => {
   res.render("index-admin", {});
 });
 
+//
+//
+////// PAGES ET ACTIONS POUR LES PIZZAS
+//
+//
 // Page de gestion des pizzas
 app.get("/pizzas", async (req, res) => {
   const allPizzas = await listPizzas();
@@ -106,7 +163,7 @@ app.post("/createpizza", async (req, res) => {
   res.redirect("/pizzas");
 });
 
-// Page édition
+// Page édition d'une pizza
 app.get("/pizzas/edit/:id", async (req, res) => {
   // Sélection du plat à modifier grâce à son id présent dans l'HTML
   const [[getPizzaToEdit]] = await promisePool.execute(
@@ -142,3 +199,78 @@ app.post("/editpizza", async (req, res) => {
   console.log(`Pizza ${pizzaLabel} modifiée`);
   res.redirect("/pizzas");
 });
+
+//
+//
+////// PAGES ET ACTIONS POUR LES LIVREURS
+//
+//
+// Page de gestion des livreurs
+app.get("/livreurs", async (req, res) => {
+    const allDM = await listDM();
+    res.render("admin-deliverymen", {
+      dMen: allDM,
+    });
+  });
+  
+  // Page d'ajout d'un nouveur livreurs
+  app.get("/createdeliveryman", async (req, res) => {
+    // permet de récupéréer tous les plats
+    //    const allDishes = await listDishes();
+    res.render("createdeliveryman", {
+      // permet d'utiliser les plats vers la pages PUG
+    });
+  });
+
+// Récupération de la requête d'ajout d'un livreur
+app.post("/createdeliveryman", async (req, res) => {
+  //recupération des informations du nouveau livreur envoyé par la méthode POST
+  //const newPizzaId = req.body.id;
+  const newDMfirstname = req.body.firstname;
+  const newDMflastname = req.body.lastname;
+  // Vérification en console
+  console.log(req.body);
+  console.log(newDMfirstname);
+  //Appel de la fonction de création de la nouvelle pizza
+  const DMAdded = await newDM(
+    newDMfirstname,
+    newDMflastname
+  );
+  //création affichée en console
+  console.log(`Nouveau livreur ajouté ${newDMfirstname}`);
+  res.redirect("/livreurs");
+});
+
+
+// Page édition d'une pizza
+app.get("/livreurs/edit/:id", async (req, res) => {
+  // Sélection du plat à modifier grâce à son id présent dans l'HTML
+  const [[getDMToEdit]] = await promisePool.execute(
+    `select * FROM delivery_men where id=?`,
+    [req.params.id]
+  );
+  // Vérification en console
+  console.log(getDMToEdit);
+  res.render("editDM", {
+    // permet d'utiliser dans pug l'objet pizza sélectionné
+    dm: getDMToEdit,
+  });
+});
+
+// Méthode de récupération du message d'édition
+app.post("/editDM", async (req, res) => {
+  const DMfirstname = req.body.firstname;
+  const DMflastname = req.body.lastname;
+  console.log(req.body);
+  const dmEdited = await editDM(
+    DMfirstname,
+    DMflastname
+  );
+  //Modification affichée en console
+  console.log(`Livreur modifié ${DMfirstname}`);
+  res.redirect("/livreurs");
+});
+
+
+
+
